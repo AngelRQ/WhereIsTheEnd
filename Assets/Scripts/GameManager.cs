1881a848b9f36e66f6e35baf1812d63dac2517f0
Maria Fernanda <mafe2112_solram@hotmail.com>
2022-11-05 15:11:41 -0600

    correccion de salto de personaje y contador

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,13 +10,16 @@ public class GameManager : MonoBehaviour
     public GameObject col;
     public List<GameObject> cols;
     public float floorSpeed = 2;
+    public float contadorTiempo = 0;
+    public Text tiempo;
 
     // Start is called before the first frame update
     void Start()
     {
         //AQUI SE CREA EL MAPA
-        for(int i=0; i<21; i++)
+        for(int i=0; i<100; i++)
         {
+            
             cols.Add(Instantiate(col, new Vector2(-10 + i, -4.5f), Quaternion.identity));
         }
     }
@@ -22,6 +27,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        contadorTiempo += Time.deltaTime;
+        tiempo.text = "" + contadorTiempo.ToString("f1");
+
         fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2 (0.10f, 0) * Time.deltaTime;
         
         //SE MUEVE EL SUELO
